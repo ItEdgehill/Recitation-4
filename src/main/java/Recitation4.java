dc740d0a68c19047cf6a1dc5e27846918bb69892
ItEdgehill <starwarsfan0125@gmail.com>
2025-02-19 21:29:50 -0500

    Complete!

--- src/main/java/Recitation4.java
+++ src/main/java/Recitation4.java
@@ -18,12 +18,35 @@ public class Recitation4 {
      */
     public static String explode(String word){
         StringBuilder sb = new StringBuilder();
+        for(int index = 0; index < word.length(); index += 1){
+            char ch = word.charAt(index);
+            if(Character.isLetter(ch)){
+                StringBuilder test = new StringBuilder(ch + "");
+                sb.append(test.repeat(test, index));
 
-        // TODO: replace this line with your code
+            }else if(Character.isDigit(ch)){
 
+                int letter_value;
+                for(letter_value = 0; (Character.getNumericValue(ch))>letter_value; letter_value++){
+                    sb.append(ch);
+                }
+            }else{
+                sb.append(word.substring(0, index));
+            }
+
+        }
         return sb.toString();
     }
 }
+
 /*
-TODO: Replace this line with your algorithm
+1. Assume we have a variable called word
+2. Assume we have a variable called sb
+3. start with first character
+4. if the character is a digit, then, duplicate based on its value
+5. else if the character is a letter, then duplicate by its position
+6. else if the character is not a letter and not a digit, then append string up to but not including
+7. get next character
+8. if there is a character, then goto 4
+9. Done
 */
